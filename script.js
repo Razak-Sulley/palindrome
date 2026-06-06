3455ae8a3647b30c8d90a6b8c229af9049fe4385
Razak-Sulley <trackmastersolutions@gmail.com>
2026-06-06 12:36:25 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("input");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function check() {
+  const value = input.value;
+  const reverse = reverseString(value);
+
+  if (value === reverse) {
+    alert("this is a palindrome");
+  } else {
+    alert("not a palindrome");
+  }
+  input.value = "";
+}
